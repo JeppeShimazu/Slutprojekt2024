90ad80d6cea3632676182f3f3a458fa65c56f3d0
Jesper Asplund <jesper.asplund@elev.ga.ntig.se>
2024-02-23 12:14:58 +0100

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+Console.WriteLine("Hej! Det här är din Tiger!");
+
+Tiger myTiger = new Tiger();
+
+Console.WriteLine("Välj ett passande namn till din Tiger! :) ");
+myTiger.name = Console.ReadLine();
+
+Console.WriteLine($",{myTiger.name}!?! Wow vilket bra namn för en Tiger");
+
+while (myTiger.GetLiving())
+{
+  Console.Clear();
+  myTiger.PrintStats();
+  Console.WriteLine($"Vad vill du göra med {myTiger.name}?");
+  Console.WriteLine($"1. Lär {myTiger.name} något nytt!");
+  Console.WriteLine($"2. Prata med {myTiger.name}!");
+  Console.WriteLine($"3. Mata {myTiger.name}!");
+  Console.WriteLine("4. Gör ingenting alls");
+
+string doWhat = Console.ReadLine();
+
+  if (doWhat == "1")
+  {
+    Console.WriteLine($"Vilket ord vill du lära {myTiger.name}?");
+    string word = Console.ReadLine();
+    //Console.WriteLine($"{myTiger.name} har lärt sig {word}!");
+    myTiger.Lär(word);
+  }
+
+  if (doWhat == "2")
+  {
+    myTiger.Hej();
+  }
+  if (doWhat == "3")
+  {
+    myTiger.Mata();
+  }
+  if (doWhat == "4")
+  {
+    Console.WriteLine("Gör ingenting.");
+  }
+//if (hunger > 9) 
+
+  Console.ReadLine();
+  myTiger.Tick();
+
+}
+//Tamagotchi dies when hunger reaches 10
+if (myTiger.GetDead())
+{
+Console.WriteLine($"NEEEEEEEJ! {myTiger.name} har dött!!!!!!");
+Console.WriteLine("Klicka ENTER för att lämna... :(");
+}
+
+Console.ReadLine();
---
+++ ConsoleApp1/TamagotchiClasses.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+public class Tiger 
+{
+private int hunger;
+private int boredom;
+private List<string> words = new List<string>() {"Gud va kul att prata!!!"};
+private bool isLiving;
+private Random generator;
+public string name;
+
+public Tiger()
+{
+generator = new Random();
+isLiving = true;
+}
+
+public void Mata()
+{
+    Console.WriteLine($"Du matar [{name}], din tiger är inte lika hungrig längre");
+    hunger -= 2;
+    if (hunger < 0)
+    {
+        hunger = 0;
+    }
+}
+
+public void Hej()
+{
+    int wordNum = generator.Next(words.Count);
+    Console.WriteLine($"[{name}] says: {words[wordNum]}");
+    ReduceBoredom();
+}
+
+public void Lär(string word)
+{
+    Console.WriteLine($" [{name}] lär sig: {word}");
+    words.Add(word);
+    ReduceBoredom();
+}
+
+
+  public void PrintStats()
+  {
+    Console.WriteLine($"Name: {name} || Hunger: {hunger} || Boredom: {boredom} || Vocabulary: {words.Count} words");
+  }
+public bool GetLiving(){
+    if (isLiving)
+    {   
+        return true;
+    }
+    return false;
+}
+
+public void ReduceBoredom()
+  {
+    Console.WriteLine($"[{name}] Är nu mindre uttråkad!!!");
+
+    boredom -= 2;
+    if (boredom < 0)
+    {
+      boredom = 0;
+    }
+  }
+
+ public void Tick()
+  {
+    hunger++;
+    boredom++;
+    if (hunger > 10 || boredom > 10)
+    {
+      isLiving = false;
+    }
+  }
+
+public bool GetDead()
+{
+    if (hunger > 9)
+    {   
+        return true;
+    }
+    return false;
+}
+
+}
+
